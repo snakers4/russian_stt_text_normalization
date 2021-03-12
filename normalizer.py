a4993f2f0b7d6a4eb41ab57108789136f40730e0
Sergey Fomin <moocher@moocher.ru>
2021-03-12 19:11:47 +0300

    Formatting fixed, unused code removed

--- normalizer.py
+++ normalizer.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import warnings
 
 
-class Normalizer():
+class Normalizer:
     def __init__(self,
                  device='cpu',
                  jit_model='jit_s2s.pt'):
@@ -26,9 +26,9 @@ class Normalizer():
         rus_letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
         spec_symbols = '¼³№¾⅞½⅔⅓⅛⅜²'
         # numbers + eng + punctuation + space + rus
-        self.src_vocab = {token: i+5 for i, token in enumerate(printable[:-5] + rus_letters + '«»—' + spec_symbols)}
+        self.src_vocab = {token: i + 5 for i, token in enumerate(printable[:-5] + rus_letters + '«»—' + spec_symbols)}
         # punctuation + space + rus
-        self.tgt_vocab = {token: i+5 for i, token in enumerate(punctuation + rus_letters + ' ' + '«»—')}
+        self.tgt_vocab = {token: i + 5 for i, token in enumerate(punctuation + rus_letters + ' ' + '«»—')}
 
         unk = '#UNK#'
         pad = '#PAD#'
@@ -49,7 +49,7 @@ class Normalizer():
         reg = re.compile(r'[^{}]+'.format(''.join(self.src_vocab.keys())))
         unk_list = re.findall(reg, string)
 
-        unk_ids = [range(m.start()+1, m.end()) for m in re.finditer(reg, string) if m.end() - m.start() > 1]
+        unk_ids = [range(m.start() + 1, m.end()) for m in re.finditer(reg, string) if m.end() - m.start() > 1]
         flat_unk_ids = [i for sublist in unk_ids for i in sublist]
 
         upd_string = ''.join([s for i, s in enumerate(string) if i not in flat_unk_ids])
@@ -93,7 +93,6 @@ class Normalizer():
                 norm_parts.append(part)
             else:
                 weighted_string = [7 if symb.isdigit() else 1 for symb in part]
-                weighted_len = sum(weighted_string)
                 if sum(weighted_string) <= self.max_len:
                     norm_parts.append(self._norm_string(part))
                 else:
@@ -105,7 +104,7 @@ class Normalizer():
                     while start_point < len(part):
                         if curr_point in spaces:
                             if sum(weighted_string[start_point:curr_point]) < self.max_len:
-                                end_point = curr_point+1
+                                end_point = curr_point + 1
                             else:
                                 norm_parts.append(self._norm_string(part[start_point:end_point]))
                                 start_point = end_point
